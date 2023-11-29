9e9468bb47bc0668d6b5a21336538b77efe8db99
Ryan Kienstra <kienstraryan@gmail.com>
2023-11-29 16:21:16 -0600

    Create this project with lein new

---
+++ README.md
@@ -0,0 +1,13 @@
+# Functional DI
+
+An example of dependency injection in Clojure.
+
+## Run unit tests
+
+```sh
+lein test
+```
+
+## License
+
+[GPLv2](https://github.com/kienstra/data-store/blob/develop/LICENSE) or later
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject functional-di "0.1.0"
+  :description "An example of using a composition root for dependency injection"
+  :url "https://github.com/kienstra/functional-di"
+  :license {:name "GPL-2.0-or-later"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt"}
+            :dependencies [[org.clojure/clojure "1.11.1"]]
+            :repl-options {:init-ns clojure-composition-root.core})
